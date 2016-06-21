b753914a372afe7af83fe3c18f698c67f8730e9a
Nadege <nadege-externe.rouelle@erdf.fr>
2016-06-21 12:02:35 +0200

    Adding fisrt test to check express is responding as expected.

---
+++ KeepItUp/test/expressspecs.ts
@@ -0,0 +1,24 @@
+﻿ /// <reference path="../Scripts/typings/mocha/mocha.d.ts"/>
+var request = require('supertest');
+
+describe("Loading express", () => {
+    var server;
+
+    beforeEach(function() {
+        server = require('../server');
+    });
+
+    afterEach(function () {
+        server.close();
+    });
+    it('responds to /', function testSlash(done) {
+        request(server)
+            .get('/')
+            .expect(200, done);
+    });
+    it('404 everything else', function testPath(done) {
+        request(server)
+            .get('/foo/bar')
+            .expect(404, done);
+    });
+});
